859152b1eb62637806551828689baf7c1b8a1803
Linzhenfeng5 <94521639+Linzhenfeng5@users.noreply.github.com>
2022-09-04 07:06:43 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -18,7 +18,7 @@ export const config = {
    *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
    *    id: "ohxOO6VskYv0Kuf-ywIZVRMpuXhk",
    *    // 你想对他发送的模板消息的模板ID
-   *    useTemplateId: "zjVglIuCXo4CsraXWEio1iP7sZJ7IQEES9lDsgc4yOU",
+   *    useTemplateId: "89WzKYIB2oJLukHYHSQlcxyWm7SviPkxYV7OrCootX4",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
    *    openUrl: "https://wangxinleo.cn"
    *   }, 
@@ -31,14 +31,14 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "ohxO123O6Vsk2Yv0Kuf-ywIZ3VRMpuXhk",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: " 	89WzKYIB2oJLukHYHSQlcxyWm7SviPkxYV7OrCootX4",
+      useTemplateId: "89WzKYIB2oJLukHYHSQlcxyWm7SviPkxYV7OrCootX4",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://www.bilibili.com/"
     }, 
     {
       name: "老婆1", 
       id: "ohxOO6VskYv20Kuf-ywIZVR3MpuXhk",
-      useTemplateId: " 	89WzKYIB2oJLukHYHSQlcxyWm7SviPkxYV7OrCootX4",
+      useTemplateId: "89WzKYIB2oJLukHYHSQlcxyWm7SviPkxYV7OrCootX4",
       openUrl: "https://baidu.com"
     }, 
     {
