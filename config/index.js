401d30ce11b8ec64b9e688465e6d3bf09b59192c
Linzhenfeng5 <94521639+Linzhenfeng5@users.noreply.github.com>
2022-09-04 08:02:14 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -29,17 +29,17 @@ export const config = {
       // 想要发送的人的名字
       name: "老婆1",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oHb9j6LkQJg-J-TzxN7a_iZbmiDk",
+      id: "oHb9j6NAwtbUSAwaQOr6WBT8pnf0",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "Sl_NUC-DKVJGbQZIJUyeQuAYOrcdTsN3xcKJu0VMisU",
+      useTemplateId: "OMSkZi05jcItGc6sxzBxOMkyLrkx7faFV5u8R7inFT8",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://www.bilibili.com/"
+      openUrl: "http://game2.baifumeiba.com/minigame/tq/?from=singlemessage&isappinstalled=0&scene=1&clicktime=1577766947&enterid=1577766947"
     }, 
     {
       name: "老婆1", 
       id: "oHb9j6LkQJg-J-TzxN7a_iZbmiDk",
-      useTemplateId: "Sl_NUC-DKVJGbQZIJUyeQuAYOrcdTsN3xcKJu0VMisU",
-      openUrl: "https://baidu.com"
+      useTemplateId: "OMSkZi05jcItGc6sxzBxOMkyLrkx7faFV5u8R7inFT8",
+      openUrl: "http://game2.baifumeiba.com/minigame/tq/?from=singlemessage&isappinstalled=0&scene=1&clicktime=1577766947&enterid=1577766947"
     }, 
     {
       name: "老婆2",
